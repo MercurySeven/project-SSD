6e4856466639dffe03f76f9a59a8cf103cec0e31
RakuJa <danielegiachetto1999@gmail.com>
2021-02-27 23:25:12 +0100

    fix #8, bugged path validation and logger getting called by himself updating the log file on disk

--- src/controller/watcher.py
+++ src/controller/watcher.py
@@ -1,8 +1,6 @@
-import time
-
 from PySide6.QtCore import Signal
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import PatternMatchingEventHandler
 
 import model.ssd_settings as ssd_settings
 
@@ -38,7 +36,7 @@ class Watcher:
                 self.background()
 
     def background(self):
-        event_handler = Handler()
+        event_handler = MyHandler()
         # Lo richiamo ogni volta perchè non posso far ripartire lo stesso
         # thread
         self.observer = Observer()
@@ -46,27 +44,58 @@ class Watcher:
         self.observer.start()
 
 
-class Handler(FileSystemEventHandler):
-    @staticmethod
-    def on_any_event(event):
-        # if event.is_directory:
-        #     return None
+class MyHandler(PatternMatchingEventHandler):
+    currentEvent = ""
+    update = False
 
-        print(
-            "[{}] noticed: [{}] on: [{}] ".format(
-                time.asctime(), event.event_type, event.src_path
-            )
-        )
-        if event.event_type == "modified" or "created" or "deleted":
-            print("Logging")
-            # first i check if getpath returns a valid pathing
-            path = ssd_settings.getpath()
-            path_is_ok = ssd_settings.validate_path(path, "log.txt")
-            if path_is_ok:
-                path = ssd_settings.setup_path(path + "log.txt")
-                with open(path, "w") as file:
-                    file.write("True")
-                    print("file modified with true")
-                    print(path)
-            else:
-                print("Path not ok")
+    def __init__(self):
+        super(
+            MyHandler,
+            self).__init__(
+            ignore_patterns=[
+                "*/log.mer",
+                "*/settings.mer"])
+
+    def log_event(self):
+        event = self.currentEvent
+        print("Logging")
+        # first i check if getpath returns a valid pathing
+        path: str = ssd_settings.getpath()
+        if path is not None:
+            path = ssd_settings.setup_path(path) + "log.mer"
+            with open(path, "a+") as file:
+                file.write(event + '\n')
+        else:
+            print("Path not ok")
+
+    def on_modified(self, event):
+        super(MyHandler, self).on_modified(event)
+        if not event.is_directory:
+            what = 'Directory' if event.is_directory else 'File'  # for future use
+            self.currentEvent = what + ", modified, " + event.src_path
+            self.log_event()
+
+    def on_created(self, event):
+        super(MyHandler, self).on_created(event)
+        if not event.is_directory:
+            what = 'Directory' if event.is_directory else 'File'
+            self.currentEvent = what + ", created, " + event.src_path
+            self.log_event()
+
+    def on_deleted(self, event):
+        super(MyHandler, self).on_deleted(event)
+        if not event.is_directory:
+            what = 'Directory' if event.is_directory else 'File'
+            self.currentEvent = what + ", deleted, " + event.src_path
+            self.log_event()
+
+    def on_moved(self, event):
+        super(MyHandler, self).on_moved(event)
+        if not event.is_directory:
+            what = 'Directory' if event.is_directory else 'File'
+            self.currentEvent = what + ", moved, from: " + \
+                event.src_path + ", to: " + event.dest_path
+            self.log_event()
+
+    def get_boolean(self, bool):
+        self.update = bool
