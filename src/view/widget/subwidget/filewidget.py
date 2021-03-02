8305d337ae11141ba77860d3dd318f4d5eec473b
Dema00 <dema199900@gmail.com>
2021-03-02 18:19:05 +0100

    Implementazione base file

--- src/view/widget/subwidget/filewidget.py
+++ src/view/widget/subwidget/filewidget.py
@@ -1,7 +1,11 @@
 from PySide6.QtWidgets import (QHBoxLayout, QWidget, QLabel)
+from PySide6 import QtCore
 class FileWidget(QWidget):
     def __init__(self, name, creation_date, last_modified_date, type, size, status):
         super(FileWidget, self).__init__()
+
+        self.setAccessibleName('File')
+        
         self.name = QLabel(str(name), self)
         self.creation_date = QLabel(str(creation_date), self)
         self.last_modified_date = QLabel(str(last_modified_date), self)
--- src/view/widget/syncronizedwidget.py
+++ src/view/widget/syncronizedwidget.py
@@ -1,10 +1,11 @@
 from PySide6.QtCore import (Signal, Slot, Qt)
-from PySide6.QtWidgets import (QLabel, QVBoxLayout, QWidget)
-from view.widget.watchwidget import WatchWidget
+from PySide6.QtWidgets import (QLabel, QVBoxLayout, QHBoxLayout, QWidget, QScrollArea, QSizePolicy)
 from model.directory import Directory
 
-from .subwidget.filewidget import FileWidget
+from view.widget.subwidget.filewidget import FileWidget
 from settings import Settings
+
+
 class SyncronizedWidget(QWidget):
 
     # creating Signals
@@ -13,20 +14,32 @@ class SyncronizedWidget(QWidget):
     def __init__(self, parent=None):
         super(SyncronizedWidget, self).__init__(parent)
 
-        self.label = QLabel("<TODO Syncronized here>", self)
-        self.label.setAlignment(Qt.AlignCenter)
         self.settings = Settings()
-        self.layout = QVBoxLayout(self)
+        layout = QVBoxLayout()
         self.current_dir = Directory('', self.settings.get_path())
         self.listOfFileWidget = []
         for file in self.current_dir.files:
             self.listOfFileWidget.append(FileWidget(file.getName(), file.getCreationDate(), file.getLastModifiedDate(), file.getType(), file.getSize(), file.getStatus()))
+        
+        self.header = FileWidget('Nome', 'Creazione', 'Ultima Modifica', 'Tipo', 'Grandezza', 'Stato Sync')
+        self.header.setAccessibleName('FileHeader')
 
-        for widget in self.listOfFileWidget:
-            self.layout.addWidget(widget)
-        self.setLayout(self.layout)
+        scrollArea = QScrollArea()
+        scrollArea.setAccessibleName('FileScroll')
+        scrollArea.setWidgetResizable(True)
+        scrollArea.horizontalScrollBar().setEnabled(False)
+
+        fileWindow = QWidget(self)
+        fileLayout = QVBoxLayout()
         # create layout
-        # layout = QVBoxLayout()
-        # layout.addWidget(self.label)
-        # self.setLayout(layout)
+
+        layout.addWidget(self.header)
+        for widget in self.listOfFileWidget:
+            fileLayout.addWidget(widget)
+            widget.setAccessibleName('File')
+        fileWindow.setLayout(fileLayout)
+        fileWindow.setParent(scrollArea)
+        scrollArea.setWidget(fileWindow)
+        layout.addWidget(scrollArea)
+        self.setLayout(layout)
 
