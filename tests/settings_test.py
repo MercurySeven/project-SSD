bf47c1d20b0bb13734d0a42002907f4c9929cb96
Davide Albiero <davide2910@live.it>
2021-03-02 17:17:44 +0100

    Aggiunti più test

--- tests/settings_test.py
+++ tests/settings_test.py
@@ -18,6 +18,12 @@ class TestSettings(unittest.TestCase):
         url = self.settings.get_server_url()
         self.assertEqual(url, "http://20.56.176.12:80/")
 
+    def test_get_server_url_with_slash(self) -> None:
+        self.settings.update_config(
+            "Connection", "address", "http://20.56.176.12/")
+        url = self.settings.get_server_url()
+        self.assertEqual(url, "http://20.56.176.12:80/")
+
     def test_get_quota_disco(self) -> None:
         quota_disco = self.settings.get_quota_disco()
         self.assertEqual(quota_disco, 1024)
@@ -39,6 +45,10 @@ class TestSettings(unittest.TestCase):
         self.settings.update_quota_disco("1000")
         self.assertEqual(self.settings.get_quota_disco(), 1000)
 
+    def test_get_quota_disco_fix(self) -> None:
+        self.settings.update_quota_disco("ABCDEFG")
+        self.assertEqual(self.settings.get_quota_disco(), 1024)
+
     def test_update_config(self) -> None:
         self.settings.update_config("Connection", "port", "22")
         url = self.settings.get_server_url()
@@ -51,6 +61,16 @@ class TestSettings(unittest.TestCase):
         self.settings.update_config("Extra", "darkmode", "True")
         feature = self.settings.get_config("Extra", "darkmode")
         self.assertEqual(feature, "True")
+
+    def test_file_already_exists(self) -> None:
+        local_settings = Settings(os.path.join("tests", "config.ini"))
+        quota_disco = local_settings.get_quota_disco()
+        self.assertEqual(quota_disco, 1024)
+
+        local_settings.update_quota_disco("123456")
+
+        quota_disco = self.settings.get_quota_disco()
+        self.assertEqual(quota_disco, 123456)
 
 
 if __name__ == "__main__":
