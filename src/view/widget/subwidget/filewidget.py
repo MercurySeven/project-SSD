552dac569a266f52671c6cbccb4df7f5622e7821
Dema00 <dema199900@gmail.com>
2021-03-03 12:06:31 +0100

    SyncWidget aggiornato

--- src/view/widget/subwidget/filewidget.py
+++ src/view/widget/subwidget/filewidget.py
@@ -1,24 +1,42 @@
-from PySide6.QtWidgets import (QHBoxLayout, QWidget, QLabel)
+from PySide6.QtWidgets import (QHBoxLayout, QWidget, QToolButton, QLabel, QVBoxLayout)
 from PySide6 import QtCore
-class FileWidget(QWidget):
+from PySide6.QtGui import QPixmap, QIcon
+from PySide6.QtCore import Qt, QSize
+
+
+class FileWidget(QToolButton):
     def __init__(self, name, creation_date, last_modified_date, type, size, status):
         super(FileWidget, self).__init__()
 
         self.setAccessibleName('File')
+
+        fileIcon = QIcon(QPixmap(':/icons/copy.png'))
+
+        self.setIcon(fileIcon)
+        self.setIconSize(QSize(50, 50))
+        self.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
         
-        self.name = QLabel(str(name), self)
-        self.creation_date = QLabel(str(creation_date), self)
-        self.last_modified_date = QLabel(str(last_modified_date), self)
-        self.type = QLabel(str(type), self)
-        self.size = QLabel(str(size), self)
-        self.status = QLabel(str(status), self)
-        self.structure = QHBoxLayout(self)
+        self.name = str(name)
+        self.creation_date = str(creation_date)
+        self.last_modified_date = str(last_modified_date)
+        self.type = str(type)
+        self.size = str(size), self
+        self.status = str(status)
+
+        self.setText(self.name)
+
+        '''self.contextWindow = QWidget()
+        self.contextWindow.nameLabel = QLabel()
+        self.contextWindow.nameLabel.setText(self.name)
+        self.contextWindow.typeLabel = QLabel()
+        self.contextWindow.typeLabel.setText(self.type)
+        contextLayout = QVBoxLayout()
+        contextLayout.addWidget(self.contextWindow.nameLabel)
+        contextLayout.addWidget(self.contextWindow.typeLabel)
+        self.contextWindow.setLayout(contextLayout)
+
+        self.setLayout(QVBoxLayout())
+        
+        self.layout().addWidget(self.contextWindow)'''
         # add fields to structure
-        self.structure.addWidget(self.name)
-        self.structure.addWidget(self.creation_date)
-        self.structure.addWidget(self.last_modified_date)
-        self.structure.addWidget(self.type)
-        self.structure.addWidget(self.size)
-        self.structure.addWidget(self.status)
-        self.setLayout(self.structure)
 
--- src/view/widget/syncronizedwidget.py
+++ src/view/widget/syncronizedwidget.py
@@ -1,9 +1,10 @@
 from PySide6.QtCore import (Signal, Slot, Qt, QSettings)
 from PySide6.QtWidgets import (
-    QLabel, QVBoxLayout, QHBoxLayout, QWidget, QScrollArea, QSizePolicy)
+    QLabel, QVBoxLayout, QHBoxLayout, QWidget, QScrollArea, QSizePolicy, QGridLayout, QPushButton)
 from model.directory import Directory
 
 from view.widget.subwidget.filewidget import FileWidget
+from view.layouts.flowlayout import FlowLayout
 import settings
 
 
@@ -19,33 +20,65 @@ class SyncronizedWidget(QWidget):
         self.env_settings = QSettings()
         self.current_dir = Directory('', self.env_settings.value("sync_path"))
         self.listOfFileWidget = []
+
+        self.fileLayout = FlowLayout()
+
+        # scroll area
+
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setAccessibleName('FileScroll')
+
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.horizontalScrollBar().setEnabled(False)
+
+        # contenitore per file
+
+        self.fileWindow = QWidget(self)
+        self.fileLayout.setContentsMargins(0,0,0,0)
+
+        self.updateButton = QPushButton(self)
+        self.updateButton.setText('Refresh')
+        self.updateButton.clicked.connect(self.updateFiles)
+
+        self.fileLayout.addWidget
+
+        self.addFiles()
+
+        self.fileWindow.setParent(self.scrollArea)
+        self.fileWindow.setLayout(self.fileLayout)
+
+        self.scrollArea.setWidget(self.fileWindow)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.updateButton)
+        layout.addWidget(self.scrollArea)
+        self.setLayout(layout)
+
+    def updateFiles(self):
+
+        for i in reversed(range(self.fileLayout.count())): 
+            self.fileLayout.itemAt(i).widget().deleteLater()
+
+        self.current_dir = Directory('', self.env_settings.value("sync_path"))
+
+        self.current_dir.files.clear()
+
+        self.current_dir.update_list_of_files()
+
+        self.addFiles()
+
+    def addFiles(self):
+
+        self.listOfFileWidget.clear()
+
         for file in self.current_dir.files:
             self.listOfFileWidget.append(FileWidget(file.get_name(), file.get_creation_date(
             ), file.get_last_modified_date(), file.get_type(), file.get_size(), file.get_status()))
 
-        self.header = FileWidget(
-            'Nome', 'Creazione', 'Ultima Modifica', 'Tipo', 'Grandezza', 'Stato Sync')
-        self.header.setAccessibleName('FileHeader')
 
-        self.scrollArea = QScrollArea()
-        self.scrollArea.setAccessibleName('FileScroll')
-        # self.scrollArea.setStyleSheet("background-color:blue;")
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollArea.setLayout(QVBoxLayout())
-        self.scrollArea.layout().setSpacing(0)
-        self.scrollArea.horizontalScrollBar().setEnabled(False)
-
-        self.fileWindow = QWidget(self)
-        fileLayout = QVBoxLayout()
-        fileLayout.setContentsMargins(0,0,0,0)
-        # create layout
-
-        layout.addWidget(self.header)
         for widget in self.listOfFileWidget:
-            fileLayout.addWidget(widget)
+            self.fileLayout.addWidget(widget)
             widget.setParent(self.fileWindow)
-        self.fileWindow.setLayout(fileLayout)
-        self.fileWindow.setParent(self.scrollArea)
-        self.scrollArea.setWidget(self.fileWindow)
-        layout.addWidget(self.scrollArea)
-        self.setLayout(layout)
+
+        
+
