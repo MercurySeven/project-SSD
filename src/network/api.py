9af8f4dbbc0857ac47c65cdc4d646b1f724bee6c
Davide Albiero <davide2910@live.it>
2021-03-06 18:20:10 +0100

    Aggiunto download e upload
    - Creato una classe apposta per recuperare i cookie, separandola dalla logica del server
    - Aggiunte le chiamate per upload e download
    - In attesa di trovare una libreria che gestisca anche i fragment ho creato un modulo che contiene solo le GraphQL Query
    - Devo ancora capire come possiamo gestire correttamente le cartelle, quindi per adesso funziona solo con la root padre

--- src/network/api.py
+++ src/network/api.py
@@ -1,164 +1,147 @@
-from requests.utils import dict_from_cookiejar
-from requests import Session
-import json
-
-
-# custum error
-class BadResponse(Exception):
-    def __init__(self, protocol, code, url):
-        super().__init__(f"Bad response: {protocol} {code} {url}")
+import os
+import logging
+import requests
+import query_model
+from datetime import datetime
+from cookie_session import CookieSession
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+from PySide6.QtCore import (QSettings)
 
 
 class API:
 
-    def __init__(self, username: str = "", password: str = ""):
+    # TODO: Da iniziare a scrive i test prima che diventi grande
 
-        self.username = username
-        self.password = password
-        self.query_endpoint = "https://mail-eu-south.testarea.zextras.com/zx/drive/graphql/v1"
-        self.up_down_endpoint = "https://mail-eu-south.testarea.zextras.com/service/extension/drive/"
-        self.web_ui = "https://mail-eu-south.testarea.zextras.com/"
-        self.session = Session()
+    def __init__(self, email: str, password: str):
+        url_base: str = "https://mail-eu-south.testarea.zextras.com/"
+        self._url_graphql: str = url_base + "zx/drive/graphql/v1/"
+        self._url_files: str = url_base + "service/extension/drive/"
 
-        if self.username and self.password:
-            self.login()
+        self._email = email
+        self._user_id = ""
+        self._env_settings = QSettings()
 
-    def upload(self):
-        # TODO
-        pass
+        session = CookieSession(email, password)
 
-    def download(self):
-        #  TODO
-        pass
+        if not session.is_logged():
+            raise ValueError("Email o password non valide")
 
-    def reset_session(self):
-        self.session = Session()
+        self._cookie = session.get_cookie_str()
 
-    def auth(self, username: str, password: str):
-        self.username = username
-        self.password = password
-        self.login()
+        _headers = {
+            "Content-Type": "application/json",
+            "cookie": self._cookie
+        }
 
-    def is_logged(self):
-        cookies = dict_from_cookiejar(self.session.cookies)
-        return True if "ZM_AUTH_TOKEN" in cookies.keys() else False
+        _transport = RequestsHTTPTransport(
+            url=self._url_graphql,
+            headers=_headers,
+            use_json=True
+        )
 
-    def csrf(self):
-        # questa chiamata setta i cookie di sessione che conterranno il codice
-        r = self.session.get(self.web_ui)
-        if not r.ok:
-            raise BadResponse("GET", r.status_code, self.web_ui)
+        self.client: Client = Client(
+            transport=_transport, fetch_schema_from_transport=True)
+        self._logger = logging.getLogger("server")
 
-        # estraggo il codice dai cookie ottenuti
-        try:
-            cookies = dict_from_cookiejar(self.session.cookies)
-            return cookies['ZM_LOGIN_CSRF']
-        except Exception:
-            raise Exception("no CSRF code found")
+    def get_info_from_email(self) -> dict[str, str]:
+        """Ritorna l'id e il nome dell'account"""
+        query = gql(query_model.get_info_from_email())
+        params = {
+            "email": self._email
+        }
+        response = self.client.execute(query, variable_values=params)
+        return response["getUserByEmail"]
 
-    def set_custom_headers(self):
+    def get_user_id(self) -> str:
+        """Metodo che recupera l'id se non scaricato in precedenza"""
+        if self._user_id == "":
+            self._user_id = self.get_info_from_email()["id"]
+        return self._user_id
+
+    def get_all_files(self) -> list:
+        """Restituisce il nome dei file con l'ultima modifica"""
+        query = gql(query_model.get_all_files())
+        params = {
+            "id": "LOCAL_ROOT"
+        }
+        response = self.client.execute(query, variable_values=params)
+        result: list = []
+        for files in response["getNode"]["children"]:
+            # Tenere questa linea fino a quando non caricheremo
+            # anche le cartelle
+            if files["type"] == "File":
+                # Bisogna dividere la data per 1000,
+                # Zextras la restituisce in millisecondi
+                # TODO: Da verificare con l'algoritmo,
+                # forse si può confrontare solo gli interi
+                # senza convertire in data
+                data = str(datetime.fromtimestamp(
+                    files["updated_at"] / 1000))
+                result.append({
+                    "id": files["id"],
+                    "name": files["name"],
+                    "updated_at": data,
+                    "size": files["size"]
+                })
+        self._logger.info(
+            f"File presenti nel server: {len(result)} files")
+        return result
+
+    def upload_to_server(self, file_path: str) -> None:
+        """Richiede il file_path"""
         headers = {
-            "Content-type": "application/json",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3",  # optional
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"  # optional
-        }
-        self.session.headers.update(headers)
-
-    def login(self):
-        if self.is_logged():
-            return True
-
-        if not (self.username and self.password):
-            raise Exception("no username and password found")
-
-        # dati estrapolati dalla chiamata post di login nel browser
-        # il codice csrf è generato dinamicamente
-        # da una chiamata get all'interfaccia web
-        login = {
-            "loginOp": "login",
-            "login_csrf": self.csrf(),
-            "username": self.username,
-            "password": self.password,
-            "client": "preferred"
+            "cookie": self._cookie
         }
 
-        r = self.session.post(self.web_ui, data=login)
-        if not r.ok:
-            raise BadResponse("POST", r.status_code, self.web_ui)
+        file_name = os.path.basename(file_path)
+        # TODO:
+        # - Da capire come gestire le cartelle
+        # - Inviare i metadati al server
+        multipart_form = {
+            "command": "upload",
+            "name": file_name,
+            "content": open(file_path, "rb"),
+            "parent": self.get_user_id() + "/LOCAL_ROOT"
+        }
 
-        # se è andata bene i nuovi cookie di sessione contengono il token di autenticazione
-        if self.is_logged():
-            # setto i nuovi headers di sessione cosi non servirà impostarli ad ogni chiamata
-            self.set_custom_headers()
-            return True
-        return False
+        response = requests.post(
+            self._url_files, headers=headers, files=multipart_form)
 
-    def call(self, query: str) -> dict:
-
-        params = {"query": query}
-
-        r = self.session.post(self.query_endpoint, json=params)
-        if not r.ok: 
-            raise BadResponse("POST", r.status_code, self.query_endpoint)
-
-        return json.loads(r.text)
-
-    def get_user_info(self, mail: str) -> dict:
-
-        query = """query {
-            getUserByEmail(email: \""""+mail+"""\") {
-                id
-                email
-                full_name
-            }
-        }"""
-
-        return self.call(query)
-
-    def get_node(self, id: str, version: int = None) -> dict:
-
-        def foo(bar):
-            print(f"fu{bar}")
-
-        if version is None:
-            query = """
-            query {
-                getNode(id: \""""+id+"""\") {
-                    id
-                    created_at
-                    name
-                    type
-                    ...cartella
-                    ...file
-                }
-            }
-
-            fragment cartella on Folder {
-                children(limit:5) {
-                    ...file
-                }
-            }
-
-            fragment file on File {
-                id
-                name
-                created_at
-                type
-                size
-                version
-                starred
-            }"""
+        if response.status_code == requests.codes.ok:
+            self._logger.info(
+                f"Upload del file {file_name}, completato con successo")
         else:
-            # TODO
-            foo("ck")
-            return {}
+            self._logger.info(f"Upload del file {file_name}, fallito")
+        return response.status_code == requests.codes.ok
 
-        return self.call(query)
+    def download_from_server(self,
+                             file_path: str,
+                             file_name: str,
+                             file_id: str) -> None:
+        """ Scarica il file dal server e lo salva nel path,
+            filename con l'estensione e path
+        """
+        headers = {
+            "cookie": self._cookie
+        }
+        url = f"{self._url_files}{self.get_user_id()}/{file_id}"
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == requests.codes.ok:
+            self._logger.info(
+                f"Download del file {file_name}, completato con successo")
+            path = os.path.join(file_path, file_name)
+            with open(path, "wb") as fh:
+                fh.write(response.content)
+        else:
+            self._logger.info(
+                f"Download del file {file_name}, fallito")
 
 
-if __name__ == "__main__":
-    api = API("mer1", "Tullio2020.")
-    print(api.is_logged())
-    print(api.get_user_info("mer1@testarea.zextras.com"))
-    print(api.get_node("LOCAL_ROOT"))
+# if __name__ == "__main__":
+#     s = API("***", "***")
+#     print(s.get_all_files())
+#     s.download_from_server(
+#         ".", "lorem.txt", "f443ca17-f0c7-48e7-9bc3-2c2f8641898a")
+#     print(s.upload_to_server("log.mer"))
