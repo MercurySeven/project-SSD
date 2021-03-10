710ea83efbe152b766a3a8303fe885871951bfa0
Davide Albiero <davide2910@live.it>
2021-03-10 18:40:49 +0100

    Eliminato la vecchia classe

--- src/network/__init__.py
+++ src/network/__init__.py
@@ -2,12 +2,10 @@ from .api import API
 from .cookie_session import (CookieSession, BadResponse)
 from .metadata import (Policy, MetaData)
 from .query_model import Query
-from .server import Server
 
 __all__ = ["API",
            "CookieSession",
            "BadResponse",
            "Policy",
            "MetaData",
-           "Query",
-           "Server"]
+           "Query"]
