0c84c684ad5f1ac4378c600b2a7955e6eb339957
Davide Albiero <davide2910@live.it>
2021-03-19 15:22:55 +0100

    Semplificato ancora di più

--- src/controllers/notification_controller.py
+++ src/controllers/notification_controller.py
@@ -1,15 +1,11 @@
-from PySide6.QtCore import (QObject, Signal)
 from PySide6.QtWidgets import (QApplication)
 from src.view.notification_view import NotificationView
 from src.view.main_view import MainWindow
 
 
-class NotificationController(QObject):
+class NotificationController:
 
-    Sg_show_app = Signal()
-
-    def __init__(self, app: QApplication, view: MainWindow, parent: QObject = None):
-        super(NotificationController, self).__init__(parent)
+    def __init__(self, app: QApplication, view: MainWindow):
 
         self.notification_view = NotificationView(app)
 
@@ -19,5 +15,4 @@ class NotificationController(QObject):
         self.notification_view.show()
 
     def send_message(self, msg: str, duration: int = 1000):
-        self.notification_view.show_message(
-            "SSD: Zextras Drive Desktop", msg, duration)
+        self.notification_view.show_message("SSD: Zextras Drive Desktop", msg, duration)
