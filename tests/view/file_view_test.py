10221d70126f542506d45d594196369331c2998e
RakuJa <danielegiachetto1999@gmail.com>
2021-03-20 11:27:18 +0100

    Add show path button test

--- tests/view/file_view_test.py
+++ tests/view/file_view_test.py
@@ -1,7 +1,8 @@
 import os
 import unittest
-
+from unittest.mock import patch
 from src import settings
+from src.controllers.file_controller import FileController
 from src.model.file_model import FileModel
 from src.view.file_view import FileView
 
@@ -13,6 +14,7 @@ class FileViewTest(unittest.TestCase):
         settings.create_standard_settings()
         self.file_model = FileModel()
         self.file_view_test = FileView(self.file_model)
+        self.file_controller = FileController(self.file_model, self.file_view_test)
 
     def tearDown(self) -> None:
         """Metodo che viene chiamato dopo ogni metodo"""
@@ -24,3 +26,8 @@ class FileViewTest(unittest.TestCase):
         self.assertEqual(self.file_view_test.title.accessibleName(), "Title")
 
         self.assertEqual(self.file_view_test.show_path_button.text(), "Apri file manager")
+
+    @patch("PySide6.QtGui.QDesktopServices.openUrl")
+    def test_show_path_button(self, mock_dialog):
+        self.file_view_test.show_path_button.click()
+        mock_dialog.assert_called_once()
