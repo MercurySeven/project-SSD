3d217fa952fe476c4204f0efa8ecc8d10c6a67cf
RakuJa <danielegiachetto1999@gmail.com>
2021-03-20 12:08:49 +0100

    Add file_widget_test, only setup and default

---
+++ tests/view/widgets/file_widget_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from src.model.file import File
+from src.view.widgets.filewidget import FileWidget
+
+
+class FileWidgetTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.file = File("nome", "creation date", "last mod date", "txt", "100", "status")
+        self.file_view_test = FileWidget(self.file)
+
+    def test_defaults(self):
+        self.assertEqual(self.file_view_test.toolTip(),
+                         "Ultima modifica: " + self.file.get_last_modified_date() + "\nSize: " + self.file.get_size())
+        self.assertEqual(self.file_view_test.text(), self.file.get_name())
