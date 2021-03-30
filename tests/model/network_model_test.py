6227dc81272c1ffd5ba4e3319262a28508e5f0e3
RakuJa <danielegiachetto1999@gmail.com>
2021-03-30 20:57:59 +0200

    Fix test network

--- tests/model/network_model_test.py
+++ tests/model/network_model_test.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 from src.model.network_model import NetworkModel
-from src.network.cookie_session import BadResponse
+from src.network.api import LoginError, NetworkError, ServerError
 from tests import default_code
 
 
@@ -18,31 +18,39 @@ class NetworkModelTest(unittest.TestCase):
         """Metodo che viene chiamato dopo ogni metodo"""
         default_code.tearDown(self.env_settings, self.restore_path)
 
-    @patch('src.network.cookie_session.CookieSession._login', return_value=False)
+    @patch('src.network.api.login', return_value=False)
     def test_login_value_exception(self, mocked_function):
         self.model_test.login("test", "test")
         self.assertEqual(False, self.model_test.is_logged())
 
-    @patch('src.network.cookie_session.CookieSession._login', return_value=False)
-    def test_login_connection_exception(self, mocked_function):
+    @patch('src.network.api.login', return_value=False)
+    def test_login_network_response_exception(self, mocked_function):
         exception_string = "Errore nella connessione con i server Zextras"
-        mocked_function.side_effect = ConnectionError(exception_string)
+        mocked_function.side_effect = NetworkError(exception_string)
         # self.assertRaises(ValueError, self.model_test.login("test", "test"))
         self.model_test.login("test", "test")
         self.assertEqual(False, self.model_test.is_logged())
         self.assertEqual(exception_string, self.model_test.get_message())
 
-    @patch('src.network.cookie_session.CookieSession._login', return_value=False)
-    def test_login_bad_response_exception(self, mocked_function):
+    @patch('src.network.api.login', return_value=False)
+    def test_login_response_exception(self, mocked_function):
+        exception_string = "Login error"
+        mocked_function.side_effect = LoginError(exception_string)
+        self.model_test.login("test", "test")
+        self.assertEqual(False, self.model_test.is_logged())
+        self.assertEqual(exception_string, self.model_test.get_message())
+
+    @patch('src.network.api.login', return_value=False)
+    def test_login_server_response_exception(self, mocked_function):
         exception_string = "Bad response: test test test"
-        mocked_function.side_effect = BadResponse("test", "test", "test")
-        # self.assertRaises(ValueError, self.model_test.login("test", "test"))
+        mocked_function.side_effect = ServerError("Server error")
         self.model_test.login("test", "test")
         self.assertEqual(False, self.model_test.is_logged())
         self.assertEqual(exception_string, self.model_test.get_message())
 
-    @patch('src.network.cookie_session.CookieSession._login', return_value=True)
-    def test_login_success(self, mocked_fun):
+    @patch('src.network.api.login', return_value=True)
+    @patch('src.network.api.is_logged', return_value=True)
+    def test_login_success(self, mocked_is_logged_in, mocked_login):
         self.model_test.login("test", "test")
-        mocked_fun.assert_called_once()
+        mocked_login.assert_called_once()
         self.assertEqual(True, self.model_test.is_logged())
