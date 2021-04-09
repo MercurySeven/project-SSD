9c849247a9ecc3f9642996c0fb3007909f7ae8f2
RakuJa <danielegiachetto1999@gmail.com>
2021-04-09 10:56:48 +0200

    Add already logged in login test

---
+++ tests/network/api_test.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import patch
+
+from src.network import api
+from tests import default_code
+
+
+class ApiTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        tmp = default_code.setUp()
+        self.restore_path = tmp[0]
+        self.env_settings = tmp[1]
+
+    def tearDown(self):
+        """Metodo che viene chiamato dopo ogni metodo"""
+        default_code.tearDown(self.env_settings, self.restore_path)
+
+    @patch('src.network.api.is_logged', return_value=True)
+    def test_login_already_logged_in(self, mocked_function):
+        result = api.login("test", "test")
+        mocked_function.assert_called_once()
+        self.assertEqual(result, True)
