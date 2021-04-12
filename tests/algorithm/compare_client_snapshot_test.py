8ff9df38a2b4d0bb6b74eb165f5433d2fb9ec22c
RakuJa <danielegiachetto1999@gmail.com>
2021-04-12 11:49:36 +0200

    Add server_new_file test

--- tests/algorithm/compare_client_snapshot_test.py
+++ tests/algorithm/compare_client_snapshot_test.py
@@ -1,8 +1,10 @@
 import unittest
 from unittest.mock import patch
 
+from src import settings
 from src.algorithm import compare_client_snapshot
 from src.algorithm.tree_comparator import Actions
+from src.model.algorithm.policy import Policy
 from tests import default_code
 from tests.default_code import _get_test_node, node_name, ResultObj
 
@@ -62,6 +64,18 @@ class CompareClientSnapshotTest(unittest.TestCase):
     @patch('src.algorithm.os_handler.upload_file')
     def test_compare_snap_client_server_new_file(self, mock_1, mock_2, mock_3):
         test_node = _get_test_node()
+        compare_client_snapshot.compare_snap_client(test_node, test_node)
+        mock_1.assert_called_once()
+        mock_2.assert_called_once()
+        mock_3.assert_called_once()
+
+    @patch('src.algorithm.tree_comparator.compareFolders',
+           return_value=ResultObj(Actions.SERVER_NEW_FILE, 1))
+    @patch('src.algorithm.compare_client_snapshot.get_id_from_path')
+    @patch('src.algorithm.os_handler.upload_file')
+    def test_compare_snap_client_server_new_update_file(self, mock_1, mock_2, mock_3):
+        test_node = _get_test_node()
+        settings.update_policy(Policy.Client)
         compare_client_snapshot.compare_snap_client(test_node, test_node)
         mock_1.assert_called_once()
         mock_2.assert_called_once()
