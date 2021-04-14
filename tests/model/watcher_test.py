f25980a1df457d432aa85103e061d64b7da9466a
RakuJa <danielegiachetto1999@gmail.com>
2021-04-14 23:56:41 +0200

    Add ugly handler tests

--- tests/model/watcher_test.py
+++ tests/model/watcher_test.py
@@ -1,3 +1,6 @@
+import os
+import pathlib
+import time
 import unittest
 from unittest.mock import patch
 
@@ -13,8 +16,27 @@ class WatcherTest(unittest.TestCase):
         self.env_settings = tmp[1]
         self.restore_credentials = tmp[2]
         self.watcher_to_test = Watcher()
+        self.path = self.env_settings.value("sync_path")
+        self.path = os.path.join(self.path, "folder")
+        pathlib.Path(self.path).mkdir(parents=True, exist_ok=True)
+        self.file_test = os.path.join(self.path, "test.txt")
 
     def tearDown(self) -> None:
+        if os.path.exists(self.file_test):
+            try:
+                os.remove(self.file_test)
+            except Exception as e:
+                print(e)
+        if os.path.exists(self.file_test + "aa"):
+            try:
+                os.remove(self.file_test + "aa")
+            except Exception as e:
+                print(e)
+        if os.path.exists(self.path):
+            try:
+                os.rmdir(self.path)
+            except Exception as e:
+                print(e)
         tearDown(self.env_settings, self.restore_path, self.restore_credentials)
 
     def test_path_change(self):
@@ -50,3 +72,32 @@ class WatcherTest(unittest.TestCase):
     def test_reboot(self, mock_run):
         self.watcher_to_test.reboot()
         self.assertEqual(mock_run.call_count, 2)
+
+    @patch('src.model.watcher.MyHandler.signal_watchdog')
+    def test_handler_create(self, mock_1):
+        self.env_settings.setValue("sync_path", self.path)
+        self.watcher_to_test.reboot()
+        with open(self.file_test, "w"):
+            pass
+        time.sleep(0.5)
+        mock_1.assert_called_once()
+
+    @patch('src.model.watcher.MyHandler.signal_watchdog')
+    def test_handler_delete(self, mock_1):
+        self.env_settings.setValue("sync_path", self.path)
+        with open(self.file_test, "w"):
+            pass
+        self.watcher_to_test.reboot()
+        os.remove(self.file_test)
+        time.sleep(0.5)
+        mock_1.assert_called_once()
+
+    @patch('src.model.watcher.MyHandler.signal_watchdog')
+    def test_handler_moved(self, mock_1):
+        self.env_settings.setValue("sync_path", self.path)
+        with open(self.file_test, "w"):
+            pass
+        self.watcher_to_test.reboot()
+        os.rename(self.file_test, self.file_test + "aa")
+        time.sleep(0.5)
+        mock_1.assert_called_once()
