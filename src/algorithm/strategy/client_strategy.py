7274cd8771588454ae4e4315ab2d3fb604b5e9f4
Davide Albiero <davide2910@live.it>
2021-04-19 15:55:00 +0200

    Fix

--- src/algorithm/strategy/client_strategy.py
+++ src/algorithm/strategy/client_strategy.py
@@ -1,5 +1,5 @@
 from logging import Logger
-from .strategy import Strategy, common_strategy
+from .strategy import Strategy, common_strategy, get_id_from_path
 from src.algorithm import os_handler
 from src.algorithm.tree_comparator import Actions
 from src.model.algorithm.tree_node import TreeNode
@@ -16,8 +16,8 @@ class ClientStrategy(Strategy):
 
             if action == Actions.SERVER_UPDATE_FILE:
                 # Il client ha un file aggiornato rispetto allo snapshot
-                path = node_raw["id"]
-                os_handler.upload_file(node, path)
+                node_id = get_id_from_path(node.get_payload().path)
+                os_handler.upload_file(node, node_id)
                 logger.info(action.name + " " + name_node)
             else:
                 common_strategy(node_raw, logger)
