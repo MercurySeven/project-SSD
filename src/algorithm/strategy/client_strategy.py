991ecac6e35337c799f3c15811f5c2ed5f384d1a
Davide Albiero <davide2910@live.it>
2021-04-19 15:41:04 +0200

    Aggiunto fix per trinity

--- src/algorithm/strategy/client_strategy.py
+++ src/algorithm/strategy/client_strategy.py
@@ -1,8 +1,23 @@
 from logging import Logger
 from .strategy import Strategy, common_strategy
+from src.algorithm import os_handler
+from src.algorithm.tree_comparator import Actions
+from src.model.algorithm.tree_node import TreeNode
 
 
 class ClientStrategy(Strategy):
     def execute(self, result_actions: list, logger: Logger) -> None:
+        # CLIENT = SNAPSHOT
+        # SERVER = CLIENT
         for node_raw in result_actions:
-            common_strategy(node_raw, logger)
+            action: Actions = node_raw["action"]
+            node: TreeNode = node_raw["node"]
+            name_node = node.get_name()
+
+            if action == Actions.SERVER_UPDATE_FILE:
+                # Il client ha un file aggiornato rispetto allo snapshot
+                path = node_raw["id"]
+                os_handler.upload_file(node, path)
+                logger.info(action.name + " " + name_node)
+            else:
+                common_strategy(node_raw, logger)
