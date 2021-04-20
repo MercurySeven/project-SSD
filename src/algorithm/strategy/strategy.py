297340b241e2489a9d5f33aaf0cc124b999bf7fa
Davide Albiero <davide2910@live.it>
2021-04-20 12:13:17 +0200

    Fix Actions.SERVER_UPDATE_FILE

--- src/algorithm/strategy/strategy.py
+++ src/algorithm/strategy/strategy.py
@@ -55,3 +55,24 @@ class Strategy(ABC):
     @abstractmethod
     def execute(self, result_actions: list, logger: Logger) -> None:
         pass
+
+    def try_get_id_from_path(self, path: str) -> str:
+        env_settings = QSettings()
+        path_folder = env_settings.value("sync_path")
+        result = os.path.relpath(path, path_folder)
+        node_name = result.split(os.sep)
+
+        current_node = tree_builder.get_tree_from_node_id()
+        index = 0
+        for name in node_name:
+            trovato = False
+            for node in current_node.get_children():
+                if node.get_name() == name:
+                    current_node = node
+                    trovato = True
+                    break
+            if not trovato and index < len(node_name) - 1:
+                id_new_folder = os_handler.create_folder(name, current_node.get_payload().id)
+                current_node = tree_builder.get_tree_from_node_id(id_new_folder)
+            index = index + 1
+        return current_node.get_payload().id
