428522182a9930e80c38897c8ebd6eaa561fbf5b
RakuJa <danielegiachetto1999@gmail.com>
2021-05-01 13:15:41 +0200

    Restore test

--- tests/view/file_view_test.py
+++ tests/view/file_view_test.py
@@ -2,6 +2,7 @@ from src.controllers.file_controller import FileController
 from src.model.main_model import MainModel
 from src.view.file_view import FileView
 from tests import default_code
+from unittest.mock import patch
 
 
 class FileViewTest(default_code.DefaultCode):
@@ -20,3 +21,11 @@ class FileViewTest(default_code.DefaultCode):
         """ Test file view test default values"""
         self.assertEqual(self.file_view_test.title.text(), "File sincronizzati")
         self.assertEqual(self.file_view_test.title.accessibleName(), "Title")
+        self.assertEqual(self.file_view_test.show_path_button.text(), "Apri file manager")
+
+    @patch("PySide6.QtGui.QDesktopServices.openUrl")
+    def test_show_path_button(self, mock_dialog):
+        """ Test if show path button calls the right method and execute
+        correctly"""
+        self.file_view_test.show_path_button.click()
+        mock_dialog.assert_called_once()
