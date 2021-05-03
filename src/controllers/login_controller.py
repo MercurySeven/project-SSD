ef79fad148be1c391f442dac66327e6f89306f15
RakuJa <danielegiachetto1999@gmail.com>
2021-05-03 22:57:27 +0200

    Fix #78

--- src/controllers/login_controller.py
+++ src/controllers/login_controller.py
@@ -22,11 +22,15 @@ class LoginController:
 
         self._net_model.Sg_model_changed.connect(self.login_screen.Sl_model_changed)
 
+        # Connetto il segnale di fallimento login
+        self._net_model.Sg_login_failed.connect(self.Sl_login_fail)
+
         user = self._net_model.get_username()
         password = self._net_model.get_password()
         if len(user) > 0 and len(password) > 0:
             # Abbiamo le credenziali salvate, facciamo l'auto-login
             self._net_model.login(user, password)
+            # Qui lancia una eccezione se il login fallisce
         else:
             self.start()
 
@@ -48,3 +52,7 @@ class LoginController:
         self.stop()
         if self._main_controller is not None:
             self._main_controller.start()
+
+    @Slot()
+    def Sl_login_fail(self):
+        self.start()
