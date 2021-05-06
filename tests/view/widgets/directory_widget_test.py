e5672112a576e5efb5f51724311aa64fd1603cb5
Dema00 <dema199900@gmail.com>
2021-05-06 13:43:54 +0200

    coverage dei widget della file view al 100%, mancano settings widget

--- tests/view/widgets/directory_widget_test.py
+++ tests/view/widgets/directory_widget_test.py
@@ -1,6 +1,9 @@
 
 from tests import default_code
 from src.view.widgets.directory_widget import DirectoryWidget
+from src.view.widgets.local_directory_widget import LocalDirectoryWidget
+from src.model.widgets.local_directory import LocalDirectory
+import time
 
 
 class DirectoryWidgetTest(default_code.DefaultCode):
@@ -23,5 +26,17 @@ class DirectoryWidgetTest(default_code.DefaultCode):
         test_result = self.test_dir.timer.isActive()
         self.assertEqual(test_result, True)
 
+    def test_check_double_click_fail_time(self):
+        self.test_dir.Sl_check_double_click()
+        time.sleep(0.250)
+        self.test_dir.Sl_check_double_click()
+        test_result = self.test_dir.timer.isActive()
+        self.assertEqual(test_result, True)
+
     def test_double_clicked_action_exists(self):
         self.test_dir.double_clicked_action()
+
+    def test_double_clicked_action_exists_local(self):
+        test_dir = LocalDirectory(default_code._get_test_node())
+        test_dir_widget = LocalDirectoryWidget(test_dir)
+        test_dir_widget.double_clicked_action()
