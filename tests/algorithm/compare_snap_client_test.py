14a382d475aed68d372d6413e730932f12b29b7e
Davide Albiero <davide2910@live.it>
2021-05-12 20:17:37 +0200

    Fix

--- tests/algorithm/compare_snap_client_test.py
+++ tests/algorithm/compare_snap_client_test.py
@@ -33,26 +33,24 @@ class CompareClientSnapshotTest(default_code.DefaultCode):
     @patch('src.algorithm.tree_comparator.compareFolders',
            return_value=ResultObj(Actions.CLIENT_NEW_FOLDER, 1))
     @patch('src.algorithm.os_handler.delete_node')
-    def test_compare_snap_client_new_folder(self, mock_1, mock_2, mock_3):
+    def test_compare_snap_client_new_folder(self, mock_1, mock_2):
         # TODO: SISTEMARE
         return
         test_node = _get_test_node()
         self.context.check(test_node, test_node, ClientStrategy())
         mock_1.assert_called_once()
         mock_2.assert_called_once()
-        mock_3.assert_called_once()
 
     @patch('src.algorithm.tree_comparator.compareFolders',
            return_value=ResultObj(Actions.CLIENT_NEW_FILE, 1))
     @patch('src.algorithm.os_handler.delete_node')
-    def test_compare_snap_client_new_file(self, mock_1, mock_2, mock_3):
+    def test_compare_snap_client_new_file(self, mock_1, mock_2):
         # TODO: SISTEMARE
         return
         test_node = _get_test_node()
         self.context.check(test_node, test_node, ClientStrategy())
         mock_1.assert_called_once()
         mock_2.assert_called_once()
-        mock_3.assert_called_once()
 
     @patch('src.algorithm.tree_comparator.compareFolders',
            return_value=ResultObj(Actions.SERVER_NEW_FOLDER, 1))
--- tests/algorithm/strategy/strategy_test.py
+++ tests/algorithm/strategy/strategy_test.py
@@ -86,7 +86,7 @@ class StrategyTest(default_code.DefaultCode):
     @patch('src.algorithm.tree_builder._build_tree_node',
            return_value=default_code._get_test_node())
     def test_execute_manual_strategy_server_update_file_diff_snap(
-            self, mock_1, mock_2, mock_3, mock_4, mock_5, mock_6):
+            self, mock_1, mock_2, mock_3, mock_4, mock_5):
         obj_to_iterate: list = [default_code.ResultObj(
             Actions.SERVER_UPDATE_FILE, 0, "name.ciao.we").result]
         logger = default_code.FakeLogger()
@@ -105,7 +105,7 @@ class StrategyTest(default_code.DefaultCode):
     @patch('src.algorithm.tree_builder._create_node_from_dict',
            return_value=default_code._get_test_node())
     def test_execute_manual_strategy_server_update_file_equal_snap(
-            self, mock_1, mock_2, mock_3, mock_4, mock_5):
+            self, mock_1, mock_2, mock_3, mock_4):
         obj_to_iterate: list = [default_code.ResultObj(
             Actions.SERVER_UPDATE_FILE, 0, "name.ciao.we", 200).result]
         logger = default_code.FakeLogger()
@@ -114,7 +114,6 @@ class StrategyTest(default_code.DefaultCode):
         mock_2.assert_called_once()
         mock_3.assert_called_once()
         mock_4.assert_called_once()
-        mock_5.assert_called_once()
 
     @patch('src.algorithm.strategy.manual_strategy.common_strategy')
     def test_execute_manual_strategy_common(self, mock_1):
